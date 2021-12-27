fe1bd2a43bc86a4e2059ed94c18a8f0a5d7bac21
unknown <hindhujhasridhar@gmail.com>
2021-12-27 21:26:00 +0530

    CA,CULATING DAILY WAGE

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,22 +6,28 @@ namespace EmployeeWage
        
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION");
             int IS_FULL_TIME = 1;
-       
+            int EMP_RATE_PER_HR = 20;
+            int empHrs = 0;
+            int empWage = 0;
 
-                Random random = new Random();
+         
+
+            Random random = new Random();
                int empcheck = random.Next(0, 2);
           
                 if (empcheck == IS_FULL_TIME)
                 {
-                    Console.WriteLine("EMPLOYEE IS PRESENT");
+                empHrs = 8;
                 }
                 else
                 {
-                    Console.WriteLine("EMPLOYEE IS ABSENT");
+                empHrs = 0;
                 }
-            }
+            empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("EMPLOYEE WAGE IS:" + empWage);
+        }
 
         }
     }
