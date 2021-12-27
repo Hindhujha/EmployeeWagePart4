94e90edbe1f5650a3e1dd811bda832c47dba4be3
unknown <hindhujhasridhar@gmail.com>
2021-12-27 22:05:31 +0530

    REFACTOR CODE BY CLASS METHOD

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -3,19 +3,19 @@ namespace EmployeeWage
 {
     class Program
     {
-       public const int IS_FULL_TIME = 1;
-       public const int IS_PART_TIME = 2;
-       public const int EMP_RATE_PER_HR = 20;
-       public const int NO_OF_WORKING_DAYS = 20;
-       public const int MAX_HRS_IN_MONTH = 100;
-        static void Main(string[] args)
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HR = 20;
+        public const int NO_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+        public static void ComputeEmpWage()
         {
             Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION");
 
             int empHrs = 0, totalEmpHrs = 0, totalworkingdays = 0;
-          
-            
-     while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalworkingdays < NO_OF_WORKING_DAYS)
+
+
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalworkingdays < NO_OF_WORKING_DAYS)
             {
                 totalworkingdays++;
                 Random random = new Random();
@@ -42,6 +42,12 @@ namespace EmployeeWage
             Console.WriteLine("TOTAL EMPLOYEE WAGE:" + totalEmpWage);
         }
 
+
+         static void Main(string[] args)
+        {
+            ComputeEmpWage();
         }
     }
+}
+
 
