daccb75a41a414b0220d9eabf024c3f6c1b542bc
unknown <hindhujhasridhar@gmail.com>
2021-12-27 21:38:21 +0530

    USING SWITCH CASE TO FIND EMPLOYEE WAGE

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -3,13 +3,13 @@ namespace EmployeeWage
 {
     class Program
     {
-       
+       public const int IS_FULL_TIME = 1;
+       public const int IS_PART_TIME = 2;
+       public const int EMP_RATE_PER_HR = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION");
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HR = 20;
+           
             int empHrs = 0;
             int empWage = 0;
 
@@ -17,18 +17,20 @@ namespace EmployeeWage
 
             Random random = new Random();
              int empcheck = random.Next(0, 3);
-          
-                if (empcheck == IS_FULL_TIME)
-                {
-                empHrs = 8;
-                }
-                else if(empcheck == IS_PART_TIME)
-                {
-                empHrs = 4;
-                }
-            else
+
+            switch (empcheck)
             {
-                empHrs = 0;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+
+                case IS_PART_TIME:
+
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HR;
             Console.WriteLine("EMPLOYEE WAGE IS:" + empWage);
